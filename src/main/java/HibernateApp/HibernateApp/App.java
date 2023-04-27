48d79c2418196f8dd1717fa8ccb92841f35f50f3
mirian228 <pariksini@mail.ru>
2023-04-27 11:02:46 +0400

    Doing first simple transaction example using Hibernate.properties, SessionFactory

--- src/main/java/HibernateApp/HibernateApp/App.java
+++ src/main/java/HibernateApp/HibernateApp/App.java
@@ -1,13 +1,35 @@
 package HibernateApp.HibernateApp;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import HibernateApp.HibernateApp.model.Person;
+
+public class App {
+	public static void main(String[] args) {
+		// Reads configuration from file named "hibernate.properties" by default
+		Configuration configuration = new Configuration();
+		// Added class for hibernate to see
+		configuration.addAnnotatedClass(Person.class);
+		// Building session factory
+		SessionFactory sessionFactory = configuration.buildSessionFactory();
+		// Getting session
+		Session session = sessionFactory.getCurrentSession();
+
+		try {
+			// Transaction Beginning
+			session.beginTransaction();
+			// Getting person with id=1 from database
+			Person person = session.get(Person.class, 1);
+			System.out.println(person.getName());
+			System.out.println(person.getAge());
+			// Committing changes from cache
+			session.getTransaction().commit();
+		} finally {
+			// Closing session factory
+			sessionFactory.close();
+		}
+
+	}
 }
--- src/main/java/HibernateApp/HibernateApp/model/Person.java
+++ src/main/java/HibernateApp/HibernateApp/model/Person.java
@@ -2,16 +2,21 @@ package HibernateApp.HibernateApp.model;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.Id;
 import javax.persistence.Table;
 
+// Entity Specifies that the class is an entity from database
 @Entity
+// Table specifies that this class is representation of the table from database
 @Table(name = "Person")
 public class Person {
-	@Column(name = "")
+	//Column annotation is used by hibernate to bind our field to database column, by name we assign name of column from database, if database column name is same as field @column annotation is not necessary
+	@Id
+	@Column(name = "id")
 	private int id;
-	
+	@Column(name = "name")
 	private String name;
-	
+	@Column(name = "age")
 	private int age;
 
 	public Person() {
@@ -22,6 +27,30 @@ public class Person {
 		super();
 		this.id = id;
 		this.name = name;
+		this.age = age;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public void setAge(int age) {
 		this.age = age;
 	}
 
